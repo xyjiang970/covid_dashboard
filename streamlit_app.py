5c58d77cadf5ec193c0331deaf9fa12c24380f32
Jason J <76984271+xyjiang970@users.noreply.github.com>
2022-01-12 13:55:22 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ url = 'https://github.com/nytimes/covid-19-data/blob/master/live/us-states.csv?r
 df = pd.read_csv(url, index_col=0)
 
 df = df[['state','cases','confirmed_cases','confirmed_deaths']]
-df = df.sort_values(by='confirmed_cases', ascending=False)
+df = df.sort_values(by='confirmed_cases', ascending=True)
 
 fig = px.bar(df.head(10), x='confirmed_cases', y='state',
              title="10 States With the Most Confirmed Covid Cases",
