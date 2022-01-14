77f2045e4631e2baf7138f30fac26ab9c49db5ef
Jason J <76984271+xyjiang970@users.noreply.github.com>
2022-01-14 17:48:06 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 import datetime
 
 # Live Datasets that are regularly updated
