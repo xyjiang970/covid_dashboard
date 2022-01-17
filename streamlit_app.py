1ffe0e572bf498738cdc5211ef0feed3464a7ef8
Jason J <76984271+xyjiang970@users.noreply.github.com>
2022-01-16 19:38:57 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,12 @@ import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import datetime
+
+import os, ssl
+
+if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
+getattr(ssl, '_create_unverified_context', None)):
+    ssl._create_default_https_context = ssl._create_unverified_context
 #############################################################################################################################
 
 # General Streamlit Adjustments
