16ef96458179ceaa41aa1eb2bb728676f59bc1ae
Jason J <76984271+xyjiang970@users.noreply.github.com>
2022-01-16 20:40:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,7 +125,7 @@ st.header('NYC Statistics')
 st.subheader('Borough Breakdown')
 st.markdown(
 """
-Confirmed data only. For "counts", the number is cumulative and sums up all confirmed cases since the beginning of the outbreak. For "rates", [NYC HEALTH](https://github.com/nychealth/coronavirus-data/tree/master/totals#by-groupcsv) defines confirmed case rate as out of 100,000 people.
+Confirmed data only. For "counts", the number is cumulative and sums up all confirmed cases _since the beginning of the outbreak_. For "rates", [NYC HEALTH](https://github.com/nychealth/coronavirus-data/tree/master/totals#by-groupcsv) defines confirmed case rate as out of 100,000 people.
 """
 )
 st.caption('Using the [by-group.csv](https://github.com/nychealth/coronavirus-data/blob/master/totals/by-group.csv) file.')
