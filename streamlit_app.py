cbec47a2e40897b92127cee1c4fd6bb823ea510a
Xin Yu (Jason) Jiang <76984271+xyjiang970@users.noreply.github.com>
2022-02-27 19:31:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -666,3 +666,17 @@ st.markdown("""
 - [Bloomberg Live Vaccine Tracker Maps & Data](https://www.bloomberg.com/graphics/covid-vaccine-tracker-global-distribution/)
 - [Johns Hopkins University Live Covid-19 Dashboard](https://coronavirus.jhu.edu/map.html)
 """)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
